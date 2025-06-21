2fefe5c1c8c1db32efe4b14808297737a88e9d8a
Sandeep Kumar <127737652+sandyyman@users.noreply.github.com>
2025-06-21 08:21:17 +0530

    Add files via upload

---
+++ Engineering Concepts/Design Patterns and Principles/Week_1_Strucutural_Patterns/5_Decorator_Notification/Code/SlackNotifierDecorator.java
@@ -0,0 +1,11 @@
+public class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send() {
+        super.send();
+        System.out.println("Sending Slack notification...");
+    }
+}
