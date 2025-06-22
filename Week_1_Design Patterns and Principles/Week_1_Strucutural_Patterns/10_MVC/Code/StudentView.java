ec0ebf6d0390e1e474e8ff5805ef99bea24e72f3
Sandeep Kumar <127737652+sandyyman@users.noreply.github.com>
2025-06-22 14:35:27 +0530

    Delete Week_1_Design Patterns and Principles directory

--- Week_1_Design Patterns and Principles/Week_1_Strucutural_Patterns/10_MVC/Code/StudentView.java
+++
@@ -1,9 +0,0 @@
-public class StudentView {
-    public void displayStudentDetails(Student student) {
-        System.out.println("Student Details:");
-        System.out.println("Name: " + student.getName());
-        System.out.println("ID: " + student.getId());
-        System.out.println("Grade: " + student.getGrade());
-        System.out.println();
-    }
-}
--- Week_1_Design Patterns and Principles/Week_1_Strucutural_Patterns/7_Observer/Code/Stock.java
+++
@@ -1,5 +0,0 @@
-public interface Stock {
-    void registerObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
