504273ce6522163cc96c36291c4eb7bd31e55764
Sandeep Kumar <127737652+sandyyman@users.noreply.github.com>
2025-07-13 19:15:24 +0530

    Add files via upload

---
+++ Week4/Load Country from Spring Configuration XML/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,40 @@
+package com.cognizant.spring_learn;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Starting application...");
+		SpringApplication.run(SpringLearnApplication.class, args);
+
+		displayCountry();
+
+		LOGGER.info("Application started.");
+	}
+
+	public static void displayCountry() {
+		LOGGER.info("START");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+		com.cognizant.springlearn.Country country = context.getBean("country", com.cognizant.springlearn.Country.class);
+
+		LOGGER.debug("Country : {}", country.toString());
+
+		LOGGER.info("END");
+	}
+}
