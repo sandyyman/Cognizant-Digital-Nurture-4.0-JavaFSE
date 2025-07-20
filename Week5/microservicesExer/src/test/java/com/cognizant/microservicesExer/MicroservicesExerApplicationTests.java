725d786016cc86d39ca6564b7318e90913467b25
Sandeep Kumar <127737652+sandyyman@users.noreply.github.com>
2025-07-20 21:53:37 +0530

    Add files via upload

---
+++ Week5/microservicesExer/src/test/java/com/cognizant/microservicesExer/MicroservicesExerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.microservicesExer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesExerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
